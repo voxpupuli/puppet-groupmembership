975c53a08868f00ddb6275677d03ae96d4297039
Zach Leslie <zach@puppetlabs.com>
2015-07-13 16:07:40 -0700

    Chomp!

--- lib/puppet/provider/groupmembership/default.rb
+++ lib/puppet/provider/groupmembership/default.rb
@@ -4,7 +4,7 @@ Puppet::Type.type(:groupmembership).provide(:default) do
   def members
     output = getent_group()
     fields = output.split(':')
-    member_list = fields[-1].split(',')
+    member_list = fields[-1].split(',').map {|i| i.chomp }
     Puppet.debug("Current membership for #{resource[:name]} is #{member_list.sort}")
     return member_list.sort
   end
