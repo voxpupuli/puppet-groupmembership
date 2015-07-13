34f3df77e0e802c290a7743329ba42df47384f0f
Zach Leslie <zach@puppetlabs.com>
2015-07-13 15:45:51 -0700

    Add some debugging

--- lib/puppet/provider/groupmembership/default.rb
+++ lib/puppet/provider/groupmembership/default.rb
@@ -5,6 +5,7 @@ Puppet::Type.type(:groupmembership).provide(:default) do
     output = getent_group()
     fields = output.split(':')
     member_list = fields[-1].split(',')
+    Puppet.debug("Current membership for #{resource[:name]} is #{member_list.sort}")
     return member_list.sort
   end
 
--- lib/puppet/provider/groupmembership/gpasswd.rb
+++ lib/puppet/provider/groupmembership/gpasswd.rb
@@ -3,10 +3,12 @@ Puppet::Type.type(:groupmembership).provide(:gpasswd, :parent => :default) do
   defaultfor :kernel => [:linux]
 
   def members=(value)
+    member_list = resource[:members].join(',').sort
+    Puppet.debug("Setting membership on #{resource[:name]} to #{member_list.sort}")
     if resource[:exclusive]
-      execute(['/usr/bin/gpasswd', '-M', resource[:members].join(','), resource[:name]], :failonfail => false)
+      execute(['/usr/bin/gpasswd', '-M', member_list, resource[:name]], :failonfail => false)
     else
-      execute(['/usr/bin/gpasswd', '-m', resource[:members].join(','), resource[:name]], :failonfail => false)
+      execute(['/usr/bin/gpasswd', '-m', member_list, resource[:name]], :failonfail => false)
     end
   end
 end
