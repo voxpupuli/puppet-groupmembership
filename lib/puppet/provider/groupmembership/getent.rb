71b2e9991ed1094b109a1005a371f445e3c55c39
Zach Leslie <zach@puppetlabs.com>
2015-07-13 19:48:06 -0700

    Improve logic for empty groups

--- lib/puppet/provider/groupmembership/getent.rb
+++ lib/puppet/provider/groupmembership/getent.rb
@@ -6,7 +6,14 @@ Puppet::Type.type(:groupmembership).provide(:getent) do
   def members
     output = getent_group()
     fields = output.split(':')
-    member_list = fields[-1].split(',').map {|i| i.chomp }.reject {|i| i.size == 0 }.sort
+
+    if fields.size < 4
+      member_list = []
+    else
+      # group:pw:gid:u1,u2
+      member_list = fields[3].split(',').map {|i| i.chomp }.reject {|i| i.size == 0 }.sort
+    end
+
     Puppet.debug("Current membership for #{resource[:name]} is #{member_list}")
     return member_list
   end
