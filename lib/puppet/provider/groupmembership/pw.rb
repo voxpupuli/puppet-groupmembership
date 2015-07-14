4329fca52bf0a3762554f0b37cd1667712f4cc67
Zach Leslie <zach@puppetlabs.com>
2015-07-13 19:09:04 -0700

    Use execute

--- lib/puppet/provider/groupmembership/pw.rb
+++ lib/puppet/provider/groupmembership/pw.rb
@@ -2,11 +2,9 @@ Puppet::Type.type(:groupmembership).provide(:pw, :parent => :default) do
   confine :kernel => [:freebsd]
   defaultfor :kernel => [:freebsd]
 
-  commands :pw => '/usr/sbin/pw'
-
   def members=(value)
     if resource[:exclusive]
-      pw(['group', 'mod', '-M', resource[:members].join(','), resource[:name]])
+      execute(['/usr/sbin/pw', 'group', 'mod', '-M', resource[:members].join(','), resource[:name]], :failonfail => false)
     else
       pw(['group', 'mod', '-m', resource[:members].join(','), resource[:name]])
     end
