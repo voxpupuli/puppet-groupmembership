e65b01d6f4455663bb0b10a41ff12fc490b3d84c
Zach Leslie <zach@puppetlabs.com>
2015-07-13 19:10:05 -0700

    Add debug, clean

--- lib/puppet/provider/groupmembership/pw.rb
+++ lib/puppet/provider/groupmembership/pw.rb
@@ -3,10 +3,12 @@ Puppet::Type.type(:groupmembership).provide(:pw, :parent => :default) do
   defaultfor :kernel => [:freebsd]
 
   def members=(value)
+    member_list = resource[:members].sort.join(',')
+    Puppet.debug("Setting membership on #{resource[:name]} to #{member_list}")
     if resource[:exclusive]
       execute(['/usr/sbin/pw', 'group', 'mod', '-M', resource[:members].join(','), resource[:name]], :failonfail => false)
     else
-      pw(['group', 'mod', '-m', resource[:members].join(','), resource[:name]])
+      execute(['/usr/sbin/pw', 'group', 'mod', '-m', resource[:members].join(','), resource[:name]], :failonfail => false)
     end
   end
 end
